87100ad93dc1da06580d0289f176c3384005651d
Michael Easter <codetojoy@gmail.com>
2013-04-09 23:17:38 -0300

    - tabs are evil

--- src/main/scala/org/peidevs/waro/Main.scala
+++ src/main/scala/org/peidevs/waro/Main.scala
@@ -4,11 +4,11 @@ package org.peidevs.waro
 import org.peidevs.waro.casino._
 
 object Main {
-	def main(args: Array[String]) {
-	    var config = new Config()
-	    var tourney = new Tourney(config)
-	    tourney.playGames()
-	    
-		println("done.")
-	}
+    def main(args: Array[String]) {
+        var config = new Config()
+        var tourney = new Tourney(config)
+        tourney.playGames()
+        
+        println("done.")
+    }
 }
